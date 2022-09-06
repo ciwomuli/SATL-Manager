a6a5c214168c7e0be3828fcc78fd724183efa370
ciwomuli <570727732@qq.com>
2022-09-06 15:15:35 +0800

    完成了结果管理

--- dataResultManager.py
+++ dataResultManager.py
@@ -7,11 +7,23 @@ class DataResultManager:
         with open(self.fileName, "r", encoding='utf8') as f:
             self.row_data = json.load(f)
 
+    def save(self):
+        with open(self.fileName, "w", encoding='utf8') as f:
+            json.dump(self.row_data, f, indent=2)
+
     def getRoundList(self):
         roundList = []
         for round in self.row_data:
             roundList.append(round)
         return roundList
+
+    def addRound(self, roundName):
+        self.row_data[roundName] = []
+        self.save()
+
+    def delRound(self, roundName):
+        del self.row_data[roundName]
+        self.save()
 
     def getResultList(self, roundName):
         resultList = []
@@ -19,8 +31,63 @@ class DataResultManager:
             resultList.append(result["home"] + " vs " + result["away"])
         return resultList
 
+    def getResult(self, roundName, index):
+        return self.row_data[roundName][index]
+
     def getDetail(self, roundName, index):
         detailList = []
         for detail in self.row_data[roundName][index]["details"]:
             detailList.append(detail)
         return detailList
+
+    def addResult(self, roundName, home, away):
+        self.row_data[roundName].append({"home": home, "away": away, "details": []})
+        self.save()
+
+    def delResult(self, roundName, index):
+        del self.row_data[roundName][index]
+        self.save()
+
+    def editResult(self, roundName, index, home, away):
+        self.row_data[roundName][index]["home"] = home
+        self.row_data[roundName][index]["away"] = away
+        self.save()
+
+    def delDetail(self, roundName, index, index2):
+        del self.row_data[roundName][index]["details"][index2]
+        self.save()
+
+    def addDetail(self, roundName, index, player1, player2, mapName, winner):
+        self.row_data[roundName][index]["details"].append({
+            "player1": player1,
+            "player2": player2,
+            "map": mapName,
+            "winner": winner
+        })
+        self.save()
+
+    def editDetail(self, roundName, index, index2, player1, player2, mapName, winner):
+        self.row_data[roundName][index]["details"][index2] = {
+            "player1": player1,
+            "player2": player2,
+            "map": mapName,
+            "winner": winner
+        }
+        self.save()
+
+    def exportResult(self, roundName, index, date):
+        result = self.row_data[roundName][index]
+        ret = "|M" + str(index + 1) + "={{Match"
+        if index == 0:
+            ret += "|dateheader=true|bestof=6|matchsection=" + roundName + "\n"
+        else:
+            ret += "\n"
+        ret += "\t|date=" + date + "{{abbr/CST}}\n"
+        ret += "\t|opponent1={{TeamOpponent|" + result["home"] + "}}\n"
+        ret += "\t|opponent2={{TeamOpponent|" + result["away"] + "}}\n"
+        for i in range(0, 6):
+            detail = result["details"][i]
+            ret += "\t|map" + str(i + 1) + "={{Map|map=" + detail["map"] + "|winner=" + str(detail["winner"]) + "|t1p1=" + \
+                   detail["player1"] + "|t2p1=" + detail["player2"] + "|subgroup=" + str(i // 2 + 1) + "}}\n"
+        ret += "}}\n"
+        return ret
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QFile
 from ui_main import Ui_MainWindow
 from dataTeamManager import *
 from dataResultManager import *
+from openpyxl import load_workbook
 
 
 # 注意 这里选择的父类 要和你UI文件窗体一样的类型
@@ -23,7 +24,18 @@ class MainWindow(QMainWindow):
         self.ui.listTeam.clicked.connect(self.clickTeam)
         self.ui.openExcelResult.clicked.connect(self.openExcelResult)
         self.ui.boxRound.currentTextChanged.connect(self.updateResult)
-        self.ui.listResult.clicked.connect(self.updateDetail)
+        self.ui.listResult.clicked.connect(self.clickResult)
+        self.ui.addResult.clicked.connect(self.addResult)
+        self.ui.delResult.clicked.connect(self.delResult)
+        self.ui.editResult.clicked.connect(self.editResult)
+        self.ui.tableDetail.clicked.connect(self.clickDetail)
+        self.ui.addDetailResult.clicked.connect(self.addDetail)
+        self.ui.delDetailResult.clicked.connect(self.delDetail)
+        self.ui.editDetailResult.clicked.connect(self.editDetail)
+        self.ui.importResult.clicked.connect(self.importResult)
+        self.ui.exportTLResult.clicked.connect(self.exportTLResult)
+        self.ui.addRound.clicked.connect(self.addRound)
+        self.ui.delRound.clicked.connect(self.delRound)
         # 初始化数据
         self.initDataTeam()
         self.initDataResult()
@@ -72,12 +84,14 @@ class MainWindow(QMainWindow):
             tablePlayer.setItem(tablePlayer.rowCount() - 1, 2, QTableWidgetItem(player["race"]))
 
     def initDataResult(self):
-        boxRound = self.ui.boxRound
         self.dataResult = DataResultManager("dataResult" + self.ui.boxCode.currentText() + ".json")
-        boxRound.clear()
-        for roundName in self.dataResult.getRoundList():
-            boxRound.addItem(roundName)
+        self.updateRound()
         self.updateResult()
+
+    def updateRound(self):
+        self.ui.boxRound.clear()
+        for roundName in self.dataResult.getRoundList():
+            self.ui.boxRound.addItem(roundName)
 
     def updateResult(self):
         roundName = self.ui.boxRound.currentText()
@@ -98,10 +112,101 @@ class MainWindow(QMainWindow):
             tableDetail.setItem(tableDetail.rowCount() - 1, 2, QTableWidgetItem(detail["map"]))
             tableDetail.setItem(tableDetail.rowCount() - 1, 3, QTableWidgetItem(detail["winner"]))
 
+    def addRound(self):
+        if len(self.ui.editRound.text()) == 0:
+            return
+        self.dataResult.addRound(self.ui.editRound.text())
+        self.updateRound()
+
+    def delRound(self):
+        self.dataResult.delRound(self.ui.boxRound.currentText())
+        self.updateRound()
+
+    def clickResult(self):
+        result = self.dataResult.getResult(self.ui.boxRound.currentText(), self.ui.listResult.currentRow())
+        self.ui.editHomeTeam.setText(result["home"])
+        self.ui.editAwayTeam.setText(result["away"])
+        self.updateDetail()
+
+    def addResult(self):
+        self.dataResult.addResult(self.ui.boxRound.currentText(), self.ui.editHomeTeam.text(),
+                                  self.ui.editAwayTeam.text())
+        self.updateResult()
+
+    def delResult(self):
+        self.dataResult.delResult(self.ui.boxRound.currentText(), self.ui.listResult.currentRow())
+        self.updateResult()
+
+    def editResult(self):
+        self.dataResult.editResult(self.ui.boxRound.currentText(),
+                                   self.ui.listResult.currentRow(),
+                                   self.ui.editHomeTeam.text(),
+                                   self.ui.editAwayTeam.text())
+        self.updateResult()
+
+    def clickDetail(self):
+        detail = self.dataResult.getDetail(self.ui.boxRound.currentText(), self.ui.listResult.currentRow())[
+            self.ui.tableDetail.currentRow()]
+        self.ui.editPlayer1.setText(detail["player1"])
+        self.ui.editPlayer2.setText(detail["player2"])
+        self.ui.editMap.setText(detail["map"])
+        self.ui.editWinner.setText(detail["winner"])
+
+    def addDetail(self):
+        self.dataResult.addDetail(self.ui.boxRound.currentText(), self.ui.listResult.currentRow(),
+                                  self.ui.editPlayer1.text(), self.ui.editPlayer2.text(), self.ui.editMap.text(),
+                                  self.ui.editWinner.text())
+        self.updateDetail()
+
+    def delDetail(self):
+        self.dataResult.delDetail(self.ui.boxRound.currentText(), self.ui.listResult.currentRow(),
+                                  self.ui.tableDetail.currentRow())
+        self.updateDetail()
+
+    def editDetail(self):
+        self.dataResult.editDetail(self.ui.boxRound.currentText(), self.ui.listResult.currentRow(),
+                                   self.ui.tableDetail.currentRow(),
+                                   self.ui.editPlayer1.text(), self.ui.editPlayer2.text(), self.ui.editMap.text(),
+                                   self.ui.editWinner.text())
+        self.updateDetail()
+
     def openExcelResult(self):
         (file_path, file_type) = QFileDialog.getOpenFileName(self, "导入结果")
         self.resultPath = file_path
+        self.excelResult = load_workbook(self.resultPath)
+        for name in self.excelResult.sheetnames:
+            self.ui.boxSheetResult.addItem(name)
 
+    def importResult(self):
+        sheet = self.excelResult[self.ui.boxSheetResult.currentText()]
+        curRow = 0
+        curCol = 0
+        for row in sheet.rows:
+            for cell in row:
+                if cell.value == self.ui.boxRound.currentText():
+                    curRow = cell.row + 2
+                    curCol = cell.column
+        while sheet.cell(curRow, curCol).value is not None:
+            self.dataResult.addResult(self.ui.boxRound.currentText(), sheet.cell(curRow, curCol).value,
+                                      sheet.cell(curRow, curCol + 4).value)
+            for i in range(1, 7):
+                self.dataResult.addDetail(self.ui.boxRound.currentText(), self.ui.listResult.count() - 1,
+                                          sheet.cell(curRow + i, curCol).value,
+                                          sheet.cell(curRow + i, curCol + 4).value,
+                                          sheet.cell(curRow + i, curCol + 2).value,
+                                          str(int(sheet.cell(curRow + i, curCol + 3).value) + 1))
+            curRow += 8
+        self.updateResult()
+        self.updateDetail()
+
+    def exportTLResult(self):
+        (file_path, file_type) = QFileDialog.getSaveFileName(self, "导出TL代码")
+        res = ""
+        for i in range(0, self.ui.listResult.count()):
+            res += self.dataResult.exportResult(self.ui.boxRound.currentText(), i, self.ui.editDate.text())
+
+        with open(file_path, "w") as f:
+            f.write(res)
 
 
 app = QApplication([])
